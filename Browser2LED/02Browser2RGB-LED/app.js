d9d9f592ad8e9b3ff1fc51f8fba31cdc2c529cca
Fabi <fabi.images@gmail.com>
2015-12-01 08:46:36 +0100

    deleted some unnecessary files

--- Browser2LED/02Browser2RGB-LED/app.js
+++ Browser2LED/02Browser2RGB-LED/app.js
@@ -1,39 +1,44 @@
-// 06: Browser --> Arduino: Control RGB LED via Web
-
-// Benötigte Module laden
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-// externe Dateien einbinden, zB css, Bilder (im public folder)
+// include external data, eg. css, images (public folder)
 app.use(express.static(__dirname + '/public'));
 
-// bei localhost:3000 erscheint index.html
-app.get('/', function(req, res){
-  res.sendfile('./public/html/index.html');
+// By typing localhost:3000 the browser will load index.html
+app.get('/', function (req, res) {
+    res.redirect('/html/index.html'); // alternative: res.sendfile('./public/html/index.html');
 });
-http.listen(3000, function(){
-  console.log('listening on Port 3000');
+
+// Put the application on port 3000
+var port = 3000;
+http.listen(port, function () {
+    console.log('listening on port ' + port);
 });
 
 // Receive Data from Browser via Socket.io and send it to Arduino via Serial Communication
-var socketio = require('socket.io')(http);
 var serialportpackage = require("serialport");
 var SerialPort = serialportpackage.SerialPort;
 var portname = "/dev/tty.usbmodemfa141";
 var serialport = new SerialPort(portname,
-{ baudrate: 9600, parser: serialportpackage.parsers.readline("\n"), dataBits: 8, parity: 'none', stopbits: 1, flowControl: false });
-serialport.open(function(){
-	console.log('Serial Port open on port ' + portname);
+    {
+        baudrate: 9600,
+        parser: serialportpackage.parsers.readline("\n"),
+        dataBits: 8,
+        parity: 'none',
+        stopbits: 1,
+        flowControl: false
+    });
+serialport.open(function () {
+    console.log('Serial Port open on port ' + portname);
 });
 
-socketio.on('connection', function(socket)
-{
-  socket.on('farbe_ClientToServer', function(data_Browser2Arduino)
-  {
-    console.log('message: ' + data_Browser2Arduino);
-	//send to Arduino
-	serialport.write(data_Browser2Arduino);
-  });
+io.on('connection', function (socket) {
+    socket.on('farbe_ClientToServer', function (data_Browser2Arduino) {
+        console.log('Color: ' + data_Browser2Arduino);
+        //send to Arduino
+        serialport.write(data_Browser2Arduino);
+    });
 });
 
